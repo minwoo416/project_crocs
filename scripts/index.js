0d4cbfab2f08886f6aae446ff2bc9786947dff41
minwoo <jmw041615@gmail.com>
2025-07-29 18:34:29 +0900

    특가세일 진행 중

---
+++ scripts/index.js
@@ -0,0 +1,52 @@
+/* 신제품 스와이퍼 */
+const newCrocs = new Swiper('.new_container .new_product_crocs',{
+    navigation: {
+        nextEl: ".new_container .swiper-button-next",
+        prevEl: ".new_container .swiper-button-prev",
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+        hide: false,
+    },
+    slidesPerView:"5",
+    spaceBetween:30,
+})
+const newJibbitz = new Swiper('.new_container .new_product_jibbitz',{
+    navigation: {
+        nextEl: ".new_container .swiper-button-next",
+        prevEl: ".new_container .swiper-button-prev",
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+        hide: false,
+    },
+    slidesPerView:5,
+})
+const newAcc = new Swiper('.new_container .new_product_acc',{
+    navigation: {
+        nextEl: ".new_container .swiper-button-next",
+        prevEl: ".new_container .swiper-button-prev",
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+        hide: false,
+    },
+    slidesPerView:5,
+})
+/* 특가세일 스와이퍼 */
+const saleWrap = new Swiper('sale_container .sale_product',{
+    navigation: {
+        nextEl: ".sale_container .swiper-button-next",
+        prevEl: ".sale_container .swiper-button-prev",
+    },
+    scrollbar: {
+        el: ".sale_container .swiper-scrollbar",
+        hide: true,
+        hide: false,
+    },
+    slidesPerView:"2",
+    spaceBetween:30,
+})
